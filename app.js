60757b2244f0d708ce17472f7c0ae15cfb02af96
Ana Luiza <demoraesraimundoa@gmail.com>
2024-09-09 17:18:24 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app.
+
+app.get('/baunilha', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public' ,'PesoIdeal_Consulta.html'));
+});
+
+app.get('/teste', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public' ,'teste.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://127.0.0.1:${port}`);
+});
